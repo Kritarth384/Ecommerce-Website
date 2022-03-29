56c3d612a0f8683a84f86cbdc032e5a686f9d6eb
Kritarth384 <kritarth20384@iiitd.ac.in>
2022-03-29 15:57:44 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react"
-import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+
+const HatsPage = () => {
+
+  return (<div>
+    <h1>HATS PAGE</h1>
+  </div>)
+};
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route path="/hats" element={<HatsPage/>} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link, useNavigate }  from "react-router-dom"
 import "./menu-item.styles.scss";
-const MenuItem = ({ title, imageUrl, size }) => (
+
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  
+
+  return(
+    
   <div className={`${size} menu-item`}>
     <div
       className="background-image"
@@ -8,11 +15,15 @@ const MenuItem = ({ title, imageUrl, size }) => (
         backgroundImage: `url(${imageUrl})`,
       }}
     ></div>
-    <div className="content">
+    <Link className="content" to={`/${linkUrl}`}>
+
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
-    </div>
+
+    </Link>
   </div>
-);
+  
+  )
+  };
 
 export default MenuItem;
