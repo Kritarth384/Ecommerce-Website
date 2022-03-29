80cc49b7b644aa3dba2b18ae58ce39ff545f03d3
Kritarth384 <kritarth20384@iiitd.ac.in>
2022-03-29 17:46:41 +0530

    header component and logo file update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component.jsx'
 
 const HatsPage = () => {
 
@@ -14,6 +15,8 @@ const HatsPage = () => {
 function App() {
   return (
     <div>
+    <Header />
+    
       <Routes>
         <Route exact path="/" element={<HomePage/>} />
         <Route path="/shop" element={<ShopPage/>} />
