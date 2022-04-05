3aace400e79a771ed455f9c88e7abea7dd566926
Kritarth384 <kritarth20384@iiitd.ac.in>
2022-04-05 10:31:53 +0530

    make persistent in content of cart and completed checkout page

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,12 +2,23 @@
 // root-reducer represent overall reducer we have in reducer
 
 import { combineReducers } from "redux";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart.reducer";
-
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+    // we only add cart to persist now 
+    // can add more later...if want
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
